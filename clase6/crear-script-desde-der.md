74693b5c975fce7edb884352b06c3dd3c21e1522
Marcos <exegeses@gmail.com>
2023-03-06 12:03:47 -0300

    correciones en el DER -  relaciones

---
+++ clase6/crear-script-desde-der.md
@@ -0,0 +1,22 @@
+# Crear Script de SQL desde un DER
+
+> Una vez que tenemos listo nuestro diagrama,
+> vamos a generar un script de SQL y en el mismo paso
+> generaremos las tablas de gráfico
+
+
+1.- En el menu ir a DATABASE -> FORWARD ENGENEER
+2.- Aparecer coadro de conexión a server
+> Nos aseguramos que está marcada la opción 
+> "Localhost"  
+
+3.- Continuar x 3 (3 veces)
+> Acá aparece el cuadro donde nos muetras el código SQL que genera el workbench
+> Si queremos guardar este código, antes de continuar, le damos al botón "Save to file"  
+ 
+4.- Continuar
+> En este paso, si no cometimos errores,
+> el workbencha acaba de crear las tablas
+
+5.- Cerrar
+
---
+++ clase6/crear-tablas-catalogo.sql
@@ -0,0 +1,31 @@
+-- Creación de tablas
+
+-- categorías
+create table categorias
+(
+    idCategoria TINYINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    catNombre VARCHAR(45) UNIQUE NOT NULL
+);
+
+-- marcas
+create table marcas
+(
+    idMarca TINYINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    mkNombre VARCHAR(45) UNIQUE NOT NULL
+);
+
+-- productos
+CREATE TABLE productos (
+               idProducto MEDIUMINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+               prdNombre VARCHAR(45) NOT NULL,
+               prdPrecio FLOAT(8,2) NOT NULL,
+               idMarca TINYINT UNSIGNED NOT NULL,
+               idCategoria TINYINT UNSIGNED NOT NULL,
+               prdDescricion VARCHAR(1000) NOT NULL,
+               prdImagen VARCHAR(45) NOT NULL,
+               prdActivo TINYINT(1) NOT NULL,
+               FOREIGN KEY (idMarca)
+                   REFERENCES marcas (idMarca),
+               FOREIGN KEY (idCategoria)
+                   REFERENCES categorias (idCategoria),
+);
